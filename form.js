019b273c0d0cdf78fc1401c4f6c61b96285136e1
joy <joy.nkwocha2003@gmail.com>
2023-05-04 09:29:07 +0100

    table is now able to pe edited and updated

--- form.js
+++ form.js
@@ -1,76 +1,89 @@
 class User {
-    constructor(firstName, lastName, email, password) {
+    constructor(firstName, lastName, email, phone) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.password = password;
-    };
-};
+        this.phone = phone;
+    }
+}
+
+const user = [];
 
 const form = document.querySelector("#main_form");
 
 const firstNameInput = document.querySelector('#name');
 const lastNameInput = document.querySelector('#last_name');
 const emailInput = document.querySelector('#email');
-const passwordInput = document.querySelector('#password');
-const confirmPasswordInput = document.querySelector('#confirm_password');
-const visibility = document.querySelector('.fa-regular');
+const phoneInput = document.querySelector('#password');
+const tableBody = document.querySelector("#table-body");
 
-const formContainer = document.querySelectorAll("#form_group");
-formContainer.forEach((container) => {
-    let children = container.children;
-    for (let child of children) {
-        child.addEventListener("input", () => {
-            if (!child.checkValidity() === true) {
-                child.classList.add("previously-focused");
-
-            } else {
-                child.classList.remove("previously-focused");
-            };
-        });
-        child.addEventListener("blur", () => {
-            if (!child.checkValidity() === true) {
-                child.classList.remove("previously-focused");
-            };
-        });
-    };
-});
-
-visibility.addEventListener("click", () => {
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-        visibility.classList.remove("fa-eye-slash");
-        visibility.classList.add("fa-eye");
-
-    } else {
-        passwordInput.type = "password";
-        visibility.classList.remove("fa-eye");
-        visibility.classList.add("fa-eye-slash");
-    };
-});
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    form.setAttribute("novalidate", "")
-    if (passwordInput.value !== confirmPasswordInput.value) {
-        confirmPasswordInput.setCustomValidity('Passwords do not match.');
+    
+    if (form.checkValidity()) {
+        let firstsName = firstNameInput.value;
+        let lastName = lastNameInput.value;
+        let email = emailInput.value;
+        let phone = phoneInput.value;
 
-    } else {
-        confirmPasswordInput.setCustomValidity('');
-    };
+        const newUser = new User(firstsName, lastName, email, phone);
+        user.push(newUser);
+        console.log(user);
 
-    confirmPasswordInput.reportValidity();
-
-    let firstsName = firstNameInput.value;
-    let lastName = lastNameInput.value;
-    let email = emailInput.value;
-    let password = passwordInput.value;
-    let confirmpassword = confirmPasswordInput.value;
-
-    const newUser = new User(firstsName, lastName, email, password, confirmpassword);
-
-    console.log(newUser.firstName);
-    form.reset();
+        addToTable(newUser);
+        form.reset();
+    }
 });
+
+function addToTable (user) {
+    const tr = document.createElement("tr");
+    const firstName = document.createElement("td");
+    firstName.textContent = user.firstName; 
+    const lastName = document.createElement("td");
+    lastName.textContent = user.lastName; 
+    const email = document.createElement("td");
+    email.textContent = user.email; 
+    const phone = document.createElement("td"); 
+    phone.textContent = user.phone; 
+    const table = document.querySelector("#table-body");
+
+    tr.appendChild(firstName);
+    tr.appendChild(lastName);
+    tr.appendChild(email);
+    tr.appendChild(phone);
+    tableBody.appendChild(tr);
+
+
+    firstName.addEventListener("click", () => {
+        const textArea = document.createElement("textarea");
+        const updateBtn = document.createElement("button");
+        updateBtn.innerText = "Update";
+        const container = document.createElement("div");
+        container.appendChild(textArea);
+        container.appendChild(updateBtn);
+        firstName.textContent = "";
+        firstName.appendChild(container);
+        textArea.value = user.firstName;
+        textArea.style.resize = 'none';
+        textArea.style.padding = 20 + "px";
+        updateBtn.addEventListener("click", (e) => {
+            e.stopPropagation();
+                firstName.textContent = textArea.value;
+                console.log(textArea.value)
+                user.firstName = textArea.value;
+                container.removeChild(textArea);
+        })
+    
+        textArea.addEventListener("click", (textAreaEvent) => {
+            textAreaEvent.stopPropagation();
+        });
+    });
+    
+};
+
+
+
+
 
 
